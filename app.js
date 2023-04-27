0278602fc8c4003655f22d5b779f01de7d382a0c
Facu_Diaz <108439711+Rackk01@users.noreply.github.com>
2023-04-27 16:48:25 -0300

    subida de la app formulario terminada

---
+++ app.js
@@ -0,0 +1,302 @@
+
+window.addEventListener("load", function () {
+
+    //*===== DEFINIMOS VARIABLES GLOBALES =====*//
+
+    let idUser;
+    let nombreUser;
+    let idLocUsuario;
+
+    //*========================================*//
+
+
+    const formulario = document.getElementById("formulario");
+
+    formulario.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        console.log(idUser);
+        console.log(nombreUser);
+        console.log(idLocUsuario);
+
+        let datos = new FormData(formulario);
+
+        if (idUser.trim() === '' || nombreUser.trim() === '' || idLocUsuario.trim() === '') {
+
+            MostrarMensaje('error', 'Oops... Ha ocurrido un error', 'Completa los campos!!');
+            return;
+        }
+
+        // actualizarDatosUsuario();
+
+    }
+
+    );
+
+
+
+
+
+    //*======================== OBTENER USUARIO ======================*//
+
+    const dataUser = async () => {
+        let formData = new FormData();
+
+
+        formData.append("funcion", "obtenerUnSoloUsuario");
+
+        try {
+
+            const res = await fetch("http://localhost/PruebaHLS2/updateform.php", {
+                method: "POST",
+                body: formData,
+            });
+
+            const data = await res.json();
+            console.log(data);
+
+            const inputUsuario = document.getElementById("idUser");
+            inputUsuario.value = data.id + "- " + data.nombres;
+
+
+
+            idUser = data.id;
+            nombreUser = data.nombres;
+            idLocUsuario = data.localidad;
+
+
+            const selectElement = document.getElementById("idLocalidad");
+            selectElement.addEventListener("change", (event) => {
+
+                idLocUsuario = event.target.value;
+                console.log(idLocUsuario); // muestra el nuevo valor de idLocUsuario después del cambio.
+            });
+
+
+            //================== MENSAJES ========================//
+
+            const alerta = document.getElementById("alerta");
+            if (data.success) {
+
+                // Mensaje estatico SUCCESS por registro exitoso del usuario en la DB
+                alerta.textContent = data.goodmessageUpdateUser;
+                alerta.classList.remove("alert-danger");
+                alerta.classList.add("alert-success");
+
+                // Mensaje pop-up SUCCESS por registro exitoso del usuario en la DB
+                Swal.fire({
+
+                    icon: 'success',
+                    title: data.goodmessageUpdateUser,
+                    text: 'que disfrutes de la estadia.',
+                })
+            } else {
+
+                // Mensaje de ERROR estatico por usuario ya registrado en la DB
+                alerta.textContent = data.message;
+                alerta.classList.remove("alert-success");
+                alerta.classList.add("alert-danger");
+
+                Swal.fire({
+
+                    // Mensaje pop-up ERROR por usuario ya registrado en la DB
+
+                    icon: 'info',
+                    title: data.message,
+                    text: 'Hola  ' + data.nombres + "  porfavor selecciona una localidad",
+                })
+
+            }
+
+            // Mostrar la alerta
+            alerta.classList.remove("d-none");
+
+            // Ocultar la alerta después de 7 segundos
+            setTimeout(() => {
+                alerta.classList.add("d-none");
+            }, 7000);
+
+        } catch (error) {
+            console.log("HAY UN ERROR  " + error);
+        }
+    };
+
+
+
+    //*===================== ACTUALIZACION DE LOCALIDAD ===================*//
+
+    const actualizarDatosUsuario = async () => {
+
+
+
+        if (idUser && nombreUser && idLocUsuario) {
+
+
+
+            //ENVIAMOS LOS DATOS POR FETCH API //
+
+            let formData = new FormData();
+            formData.append("funcion", "actualizarUsuario");
+            formData.append("idUser", idUser);
+            formData.append("nombreUser", nombreUser);
+            formData.append("idLocUsuario", idLocUsuario);
+
+
+
+
+            try {
+
+                // ESPERAMOS RESPUESTA DEL FETCH EN FORMATO JSON //
+
+                const res = await fetch("http://localhost/PruebaHLS2/updateform.php", {
+                    method: "POST",
+                    body: formData,
+                });
+
+                const data = await res.json(); //cambiar a json.
+                console.log(data);
+
+                //===============================================//
+
+                //=================== MENSAJES ================//
+
+                const alerta = document.getElementById("alerta");
+                if (data.success) {
+
+                    // Mensaje estatico SUCCESS por registro exitoso del usuario en la DB
+                    alerta.textContent = data.goodmessageUpdateUser;
+                    alerta.classList.remove("alert-danger");
+                    alerta.classList.add("alert-success");
+
+                    // Mensaje pop-up SUCCESS por registro exitoso del usuario en la DB
+                    Swal.fire({
+
+                        icon: 'success',
+                        title: data.goodmessageUpdateUser,
+                        text: 'que disfrutes de la estadia en  ' + idLocalidad,
+                    })
+                } else {
+
+                    // Mensaje de ERROR estatico por usuario ya registrado en la DB
+                    alerta.textContent = data.message;
+                    alerta.classList.remove("alert-success");
+                    alerta.classList.add("alert-danger");
+
+                    Swal.fire({
+
+                        // Mensaje pop-up ERROR por usuario ya registrado en la DB
+
+                        icon: 'error',
+                        title: data.message,
+                        text: nombreUser + "  Oops! Hubo algun error.",
+                    })
+
+                }
+
+            } catch (error) {
+
+                console.error(error);
+            }
+
+        } else {
+
+            console.log("CHE MAQUINA TENES UN ERROR!!");
+        }
+
+
+    }
+
+
+    //*====================== OBTENER LOCALIDADES ===================*//
+
+    const getLocalidades = async () => {
+        let formData = new FormData();
+        formData.append("funcion", "obtenerTodasLocalidades");
+
+
+
+        try {
+
+            const res = await fetch("http://localhost/PruebaHLS2/updateform.php", {
+                method: "POST",
+                body: formData,
+            });
+
+            const data = await res.json();
+            console.log(data);
+
+            const localidad = document.getElementById("idLocalidad");
+            for (let locali of data) {
+                console.log(locali.id, locali.nombre);
+
+                const option = document.createElement("option");
+                option.value = locali.id;
+                option.text = locali.nombre;
+                localidad.add(option);
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    /* 
+    MENSAJES DE ALERTA.
+
+    colocar mensajes de alerta al finalizar correctamente la app
+
+    */
+
+
+    function MostrarMensaje(icono, titulo, texto) {
+        const alerta = document.getElementById("alertaInfo");
+
+        Swal.fire({
+            // Mensaje pop-up de error por inputs vacios (mensaje manual)
+            icon: icono,
+            title: titulo,
+            text: texto,
+
+        });
+
+        // 1 Mostrar el alert bootstrap con las clases y mensajes correspondientes
+
+        if (icono == "error") {
+
+
+
+            alerta.textContent = texto;
+            alerta.classList.remove("alert-success");
+            alerta.classList.remove("alert-info");
+            alerta.classList.add("alert-danger");
+
+            document.getElementById('idParrafo').innerHTML = texto;
+        } else {
+
+            alerta.textContent = texto;
+            alerta.classList.remove("alert-danger");
+            alerta.classList.remove("alert-info");
+            alerta.classList.add("alert-success");
+        }
+
+        //Mostrar la alerta en pantalla
+        alerta.classList.remove("d-none");
+
+        //Ocultar la alerta después de 7 segundos
+        setTimeout(() => {
+            alerta.classList.add("d-none");
+        }, 7000);
+
+
+    }
+
+
+
+    //#region Llamadas a métodos
+    dataUser();
+    actualizarDatosUsuario();
+    getLocalidades();
+
+    //#endregion
+
+})
---
+++ updateform.php
@@ -0,0 +1,139 @@
+<?php
+session_start();
+
+$funcion = $_POST["funcion"];
+
+
+
+//CONEXION A LA BASE DE DATOS.
+
+
+$host = "localhost";
+$port = "5435";
+$dbname = "db_prueba";
+$user = "postgres";
+$password = "113355";
+
+$conex = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+if (!$conex) {
+
+    die("error de conexion: " . pg_last_error());
+}
+
+
+
+switch ($funcion) {
+
+
+    case "obtenerTodasLocalidades":
+
+        //===================== OBTENER LOCALIDADES ================//
+
+        $opciones = array();
+        $query = "SELECT * FROM localidades";
+        $resultado = pg_query($conex, $query);
+
+        if (pg_num_rows($resultado) > 0) {
+
+            $localidades = pg_fetch_all($resultado);
+            foreach ($localidades as $localidad) {
+                $opciones[] = array("id" => $localidad["id"], "nombre" => $localidad["nombre"]);
+            }
+            echo json_encode($opciones);
+        } else {
+
+            $opciones = array(array("id" => "", "nombre" => "No se encontraron opciones disponibles"));
+        }
+
+        break;
+
+        //==================== FIN OBTENER LOCALIDADES ==============//
+
+
+
+
+
+
+    case "obtenerUnSoloUsuario":
+
+        //===================== OBTENER REGISTRO USUARIO ==============//
+
+        $idUsuario = array();
+        $query = "SELECT * FROM usuarios LIMIT 1";
+        $resultadox = pg_query($conex, $query);
+
+
+        // Verificar si la consulta se ejecutó correctamente
+        if ($resultadox) {
+            // Obtener los datos de cada fila de resultadox
+            while ($fila = pg_fetch_assoc($resultadox)) {
+
+                // Agregar TODOS los datos de la fila al array $idUsuario
+                $idUsuario[] = $fila;
+            }
+        } else {
+            // Mostrar un mensaje de error si la consulta no se ejecutó correctamente
+            echo "Error en la consulta: " . pg_last_error($conex);
+        }
+
+        // Desglosar los datos del array $idUsuario y enviar datos en formato json.
+        foreach ($idUsuario as $usuario) {
+
+            echo json_encode($usuario);
+        }
+        break;
+
+        //=================== FIN OBTENER REGISTRO USUARIO ===================//
+
+
+ 
+
+
+    case "actualizarUsuario":
+
+
+        //===================== ACTUALIZACION DE LOCALIDAD ===================//
+
+
+        $idUser = $_POST["idUser"];
+        $nombreUser = $_POST["nombreUser"];
+        $idLocUsuario = $_POST["idLocUsuario"];
+
+
+        // Validar que las variables no esten vacias
+
+        if (!isset($idUser) || !isset($nombreUser) || !isset($idLocUsuario)) {
+            echo json_encode("Error: UNO O MAS VALORES SON UNDEFINED");
+            break;
+        }
+
+
+        $query = "UPDATE usuarios SET localidad = '$idLocUsuario' WHERE id = $idUser";
+        $resultadoUpdate = pg_query($conex, $query);
+
+        //==================== MENSAJES ==================//
+
+        if ($resultadoUpdate) {
+            // Mensaje de actualización fue exitosa.
+            $resultado1 = array("success" => true, "goodmessage" => "Actualización exitosa.");
+            echo json_encode($resultado1);
+        } else {
+            // Mensaje de error en la conexión.
+            $resultado1 = array("success" => false, "message" => "Hubo algún error en la actualización.");
+            echo json_encode($resultado1);
+            return;
+        }
+
+        break;
+        //=================== FIN ACTUALIZACION DE LOCALIDAD =================// 
+
+
+
+
+    default:
+
+        // código a ejecutar si no se cumple ninguno de los casos anteriores
+        $resultado = array("success" => false, "messageErr" => "Acción no válida.");
+        echo json_encode($resultado);
+        break;
+}
